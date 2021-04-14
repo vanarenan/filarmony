a560a0d82f15285d8bfd017fd5ca3332949da9d8
vbotezat <vbotezat@lab-code.com>
2021-04-15 02:59:21 +0300

    deploy

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2bb09ed5bc1ce38a4fc4cab9f6dfbdb.js"
+  "/filarmony/precache-manifest.4154eddf1e388956173d8c6bfb15ab54.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/filarmony/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
